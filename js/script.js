550a2e2398de7c8054008c9a621e12f8afd592b9
Paulo <trigopaulo@gmail.com>
2022-09-05 22:53:50 -0300

    js ended

--- js/script.js
+++ js/script.js
@@ -27,6 +27,43 @@ function start() {
     breakCount = 0;
 
     let timerFunction = () => {
-        
+        document.getElementById('minutes').innerHTML = workMinutes;
+        document.getElementById('seconds').innerHTML = seconds;
+    
+        seconds = seconds - 1;
+
+        if(seconds === 0){
+            workMinutes = workMinutes -1;
+            
+            if(workMinutes === -1){
+
+                if(breakCount%2 === 0){
+
+                    //Começar o descanso
+                    workMinutes = breakMinutes;
+                    breakCount++;
+
+                    //trocar para o pomodoro de trabalho
+                    workTittle.classList.remove('active');
+                    breakTime.classList.add('active');
+                }
+                else
+                {
+                    
+                    //continuar o funcionamento do código
+                    workMinutes = workTime;
+                    breakCount++
+
+                    //vai trocar para o Pomodoro de trabalho
+                    breakTittle.classList.remove('active');
+                    workTittle.classList.add('active');
+                }
+            }
+
+            seconds = 59;
+        }
     }
+
+    setInterval(timerFunction, 1000);
+    //1000 == 1s
 }
